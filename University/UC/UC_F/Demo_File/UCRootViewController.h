18867ca9c11eb6344887bd5aafe9326f0b877c6c
王坤成 <18501987683@163.com>
2016-09-03 10:40:59 +0800

    1
    1

---
+++ University/UC/UC_F/Demo_File/UCRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  UCRootViewController.h
+//  University
+//
+//  Created by Talkmate on 16/9/3.
+//  Copyright © 2016年 CocoaKC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UCRootViewController : UIViewController
+
+@end
---
+++ University/UC/UC_F/Demo_File/UCRootViewController.m
@@ -0,0 +1,28 @@
+//
+//  UCRootViewController.m
+//  University
+//
+//  Created by Talkmate on 16/9/3.
+//  Copyright © 2016年 CocoaKC. All rights reserved.
+//
+
+#import "UCRootViewController.h"
+
+@interface UCRootViewController ()
+
+@end
+
+@implementation UCRootViewController
+
+#pragma mark - LifeCycle
+
+- (void)viewDidLoad {
+
+    [super viewDidLoad];
+    
+    
+    
+ 
+}
+
+@end
